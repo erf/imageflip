649bf9ea106d3d9dc86763bdadb5f3e381838414
Erlend Fagerheim <erlendf80@gmail.com>
2013-05-04 19:55:19 +0200

    src is now called imageflip.js, added minified imageflip.min.js

--- imageflip.js
+++ imageflip.js
@@ -1,1 +1,27 @@
-function ImageFlip(t,e,n){function r(){i=(i+1)%l.length,o.src=l[i]+"?v="+(new Date).getTime()}function a(){c&&clearTimeout(c),c=setInterval(r,u)}var c,i=0,u=n||2500,l=e,o=t;return o.src=l[0],{start:a}}
+function ImageFlip(p_el, p_urls, p_time){
+
+    var id, i,
+        time = p_time || 2500,
+        urls = p_urls,
+        el = p_el;
+
+    function start() {
+        i = 0;
+        el.src = urls[i];
+
+        if(id) {
+            clearTimeout(id);
+        }
+
+        function flip(){
+            i = (i+1) % urls.length;
+            el.src = urls[i] + '?v=' + new Date().getTime();   // http://goo.gl/QtnmT
+        }
+
+        id = setInterval(flip, time);
+    }
+
+    return {
+        start: start
+    }
+}
